2be904c47e79653ed4f4915ca9c8be9e0dd4d2a0
kaif <144448294+katamsyi@users.noreply.github.com>
2025-05-29 13:17:28 +0800

    cobaz

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 });
 
 // Jalankan server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 db.sync({ force: true }) // ini akan hapus dan buat ulang tabel sesuai model
   .then(() => {
     app.listen(PORT, () => {
